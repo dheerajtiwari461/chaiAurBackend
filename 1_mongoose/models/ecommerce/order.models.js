21110927fef491f9cc33bb40c077bd11ab0a23ec
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-06-03 13:02:38 +0530

    feat: Add .prettierignore, .prettierrc, and package.json files
    This commit adds the `.prettierignore`, `.prettierrc`, and `package.json` files to the backend directory. The `.prettierignore` file specifies the files and directories to be ignored by the Prettier code formatter. The `.prettierrc` file contains the configuration options for Prettier, such as single quotes, bracket spacing, tab width, trailing comma, and semicolon usage. The `package.json` file includes the project's name, version, description, main file, scripts, author, license, and devDependencies.

--- 1_mongoose/models/ecommerce/order.models.js
+++ 1_mongoose/models/ecommerce/order.models.js
@@ -1,18 +1,41 @@
 import mongoose from "mongoose";
 
 const orderItemsSchema = new mongoose.Schema({
-    p
-})
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    required: true,
+  },
+});
 
-const orderSchema = new mongoose.Schema({
-    orderPrice:{
-        type: Number,
-        required: true
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
     },
-    customerId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    }
-},{timestamps: true});
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    orderItems: {
+      type: [orderItemsSchema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "CANCELLED", "DELIVERED"],
+      default: "PENDING",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Order = mongoose.model("Order", orderSchema);
