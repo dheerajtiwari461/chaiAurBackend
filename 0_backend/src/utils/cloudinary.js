67c81f45521c456c123dc9042f93b1ece2c02877
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-06-06 09:42:56 +0530

    feat: Update Cloudinary configuration and add image upload function
    The code changes in this commit update the Cloudinary configuration in the `cloudinary.js` file to use environment variables for the cloud name, API key, and API secret. This improves security by not exposing sensitive information in the code. Additionally, a new function `uploadOnCloudinary` is added to handle the upload of images to Cloudinary. This function takes a local file path as input, uploads the file to Cloudinary, and returns the response URL.

--- 0_backend/src/utils/cloudinary.js
+++ 0_backend/src/utils/cloudinary.js
@@ -2,7 +2,25 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({ 
-    cloud_name: "dbq21hjjg", 
-    api_key: "631967737199921", 
-    api_secret: "<your_api_secret>" // Click 'View Credentials' below to copy your API secret
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        // upload on cloudinary 
+        const response = await  cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto",
+        })
+        // file has been uploaded on cloudinary
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+};
+
+export {uploadOnCloudinary}; //export the function for use in other files
