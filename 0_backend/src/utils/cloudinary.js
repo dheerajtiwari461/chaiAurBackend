f63e3d553899d6186d1db084991fa53af46d78fd
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-06-06 06:53:24 +0530

    feat: Add Cloudinary configuration for image management

---
+++ 0_backend/src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+    cloud_name: "dbq21hjjg", 
+    api_key: "631967737199921", 
+    api_secret: "<your_api_secret>" // Click 'View Credentials' below to copy your API secret
+});
