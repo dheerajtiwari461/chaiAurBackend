17cdee7f4d12e13cb054f49644e0871039e01e5b
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-06-06 17:09:52 +0530

    all set configured

--- 0_backend/src/app.js
+++ 0_backend/src/app.js
@@ -1,6 +1,6 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -14,5 +14,15 @@ app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({express: true, limit: '16kb'}));
 app.use(express.static('public'));
 app.use(cookieParser());
+
+
+//routes import 
+import userRoutes from "./routes/user.routes.js";
+
+// routes delcration
+app.use("/api/v1/users", userRoutes);
+
+// http://localhost:8000/api/v1/users/register
+
 
 export {app};
